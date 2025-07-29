ff9c6f44daec6ac31e1397c1ca95b1a5c56fbcb0
captain-woof-optiv <sohail.saha@optiv.com>
2025-07-29 17:20:35 +0530

    csv feature added

--- nmap-to-list.py
+++ nmap-to-list.py
@@ -20,11 +20,13 @@ parser.add_argument("-n", "--nmap-xml", action="store", help="Nmap XML")
 parser.add_argument("--host-to-port", action="store", help="Output file for HOST:PORT; default: 'host_to_port.txt'", default="host_to_port.txt")
 parser.add_argument("--host-to-port-web", action="store", help="Output file for HOST:PORT for web (http and https); default: 'host_to_port_web.txt'", default="host_to_port_web.txt")
 parser.add_argument("--urls-base", action="store", help="Output file for http://HOST:PORT; default: 'urls_base.txt'", default="urls_base.txt")
+parser.add_argument("--csv", action="store", help="Output file for CSV; default: 'nmap.csv'", default="nmap.csv")
 args = parser.parse_args()
 
 hostToPortFileName = args.host_to_port
 hostToPortWebFileName = args.host_to_port_web
 webUrlsFileName = args.urls_base
+csvFilePath = args.csv
 nmapXmlFilePath = args.nmap_xml
 
 # Take XML as input
@@ -36,30 +38,65 @@ hosts = root.findall("host")
 hostToPort = [] # HOST:PORT
 hostToPortWeb = [] # HOST:PORT
 webUrls = [] # https://HOST:PORT
+csvContents = ["ip,port,service_type,service_name,device_type,tls_subject,http_title,notes"]
 
 for host in hosts:
     address = host.find("address").attrib["addr"]
     portsElements = host.find("ports").findall("port")
 
     for portElement in portsElements:
+        port = portElement.attrib["portid"]
+
+        # Process services
+        serviceName = ""
+        serviceProduct = ""
+        serviceVersion = ""
+        serviceExtraInfo = ""
+        serviceDeviceType = ""
+
         serviceElement = portElement.find("service")
         if serviceElement is not None:
-            serviceName = serviceElement.attrib["name"]
-            if serviceName is not None:
-                port = portElement.attrib["portid"]
+            serviceName = serviceElement.attrib["name"] or ""
+            serviceProduct = serviceElement.attrib["product"] or ""
+            serviceVersion = serviceElement.attrib["version"] or ""
+            serviceExtraInfo = serviceElement.attrib["extrainfo"] or ""
+            serviceDeviceType = serviceElement.attrib["devicetype"] or ""
 
-                # Web host -> web port mapping (https)
-                if serviceName.lower() == "https":
-                    webUrls.append(f"https://{address}:{port}")
-                    hostToPortWeb.append(f"{address}:{port}")
+        # Process scripts
+        scriptTlsSubject = ""
+        scriptHttpTitle = ""
 
-                # Web host -> web port mapping (http)
-                elif serviceName.lower() == "http":
-                    webUrls.append(f"http://{address}:{port}")
-                    hostToPortWeb.append(f"{address}:{port}")
+        scriptElements = portElement.findall("script")
+        for scriptElement in scriptElements:
+            if scriptElement is not None:
+                # TLS/SSL certificate
+                if scriptElement.attrib["id"].lower() == "ssl-cert":
+                    scriptTlsSubject = scriptElement.attrib["output"] or ""
 
-                # Generic host -> port mapping
-                hostToPort.append(f"{address}:{port}")
+                # HTTP title
+                elif scriptElement.attrib["id"].lower() == "http-title":
+                    scriptHttpTitle = scriptElement.attrib["output"] or ""
+
+                # http-ntlm-info
+
+        # Store results
+
+        ## Web host -> web port mapping (https)
+        if serviceName.lower() == "https":
+            webUrls.append(f"https://{address}:{port}")
+            hostToPortWeb.append(f"{address}:{port}")
+
+        ## Web host -> web port mapping (http)
+        elif serviceName.lower() == "http":
+            webUrls.append(f"http://{address}:{port}")
+            hostToPortWeb.append(f"{address}:{port}")
+
+        ## CSV
+        csvContents.append(f"{address},{port},{serviceName},{" ".join([serviceProduct,serviceVersion,serviceExtraInfo])},{serviceDeviceType},{scriptTlsSubject},{scriptHttpTitle},todo")
+
+        ## Generic host -> port mapping
+        hostToPort.append(f"{address}:{port}")
+            
 
 # Output files
 with open(hostToPortFileName, "a") as fileToWrite:
@@ -76,3 +113,8 @@ with open(webUrlsFileName, "a") as fileToWrite:
     for lineToWrite in webUrls:
         fileToWrite.write(lineToWrite + "\n")
     print(f"http://HOST:PORT list written to '{webUrlsFileName}'")
+
+with open(csvFilePath, "a") as fileToWrite:
+    for lineToWrite in csvContents:
+        fileToWrite.write(lineToWrite + "\n")
+    print(f"CSV written to '{csvFilePath}'")
